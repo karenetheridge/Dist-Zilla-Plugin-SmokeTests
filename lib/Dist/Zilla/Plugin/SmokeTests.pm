d7072be886743ae9e805b4f4cacd82a428cfe8ac
Karen Etheridge <ether@cpan.org>
2016-03-16 20:35:54 -0700

    increment $VERSION after 0.002 release

--- lib/Dist/Zilla/Plugin/SmokeTests.pm
+++ lib/Dist/Zilla/Plugin/SmokeTests.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::SmokeTests;
 # ABSTRACT: Tell smoke testers to run your smoke tests
 # KEYWORDS: makemaker smoke smoker tests testing automated
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 use constant DEFAULT_FINDER => 'xt/smoke/*.t';
 
